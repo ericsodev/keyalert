de009eb6242b36de1ea59a69820e21a7b76429d0
Eric <eric.so.dev@gmail.com>
2024-12-27 00:28:13 -0500

    feat: command to create migration and seed files

---
+++ packages/functions/db/.config/kysely.config.ts
@@ -0,0 +1,31 @@
+import {
+  DummyDriver,
+  PostgresAdapter,
+  PostgresIntrospector,
+  PostgresQueryCompiler,
+} from "kysely";
+import { defineConfig } from "kysely-ctl";
+
+export default defineConfig({
+  dialect: {
+    createAdapter() {
+      return new PostgresAdapter();
+    },
+    createDriver() {
+      return new DummyDriver();
+    },
+    createIntrospector(db) {
+      return new PostgresIntrospector(db);
+    },
+    createQueryCompiler() {
+      return new PostgresQueryCompiler();
+    },
+  },
+  migrations: {
+    migrationFolder: "migrations",
+  },
+  //   plugins: [],
+  seeds: {
+    seedFolder: "seeds",
+  },
+});
