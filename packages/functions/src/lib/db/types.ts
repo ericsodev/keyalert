961fb06967511e65f2d6dbb70c37384c224d8bc0
Eric <eric.so.dev@gmail.com>
2024-12-29 17:50:12 -0500

    feat: ingest batch table

--- packages/functions/src/lib/db/types.ts
+++ packages/functions/src/lib/db/types.ts
@@ -1,7 +1,8 @@
-import { ColumnType, Generated } from "kysely";
+import { ColumnType, GeneratedAlways } from "kysely";
 
 export interface Database {
   ingest_log: IngestLogTable;
+  ingest_batch: IngestBatchTable;
 }
 
 export enum IngestSource {
@@ -9,15 +10,28 @@ export enum IngestSource {
   MANUAL = "Manual",
 }
 
+export enum IngestTargetType {
+  KEYBOARD = "Keyboard",
+}
+
 interface BaseTable {
-  id: Generated<string>;
+  id: GeneratedAlways<string>;
   createdAt: ColumnType<Date, never, never>;
   updatedAt: ColumnType<Date, never, string | undefined>;
-  deletedAt: ColumnType<Date, string | undefined, string | undefined>;
+  deletedAt: ColumnType<Date, never, string | undefined>;
+}
+
+export interface IngestBatchTable extends BaseTable {
+  source: IngestSource;
+  timestamp: ColumnType<Date, string | undefined, string | undefined>;
+  triggeredManually: boolean;
+  startDate: ColumnType<Date, string, undefined>;
+  endDate: ColumnType<Date, string, undefined>;
 }
 
 export interface IngestLogTable extends BaseTable {
   label: string;
+  type: IngestTargetType;
   source: IngestSource;
   externalId: string;
   timestamp: ColumnType<Date, string | undefined, string | undefined>;
