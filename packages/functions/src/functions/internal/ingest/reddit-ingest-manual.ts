d455de23c211731e59d9da7be5c5cccc0693b437
Eric <eric.so.dev@gmail.com>
2025-01-02 00:02:35 -0500

    feat: testing ingest

--- packages/functions/src/functions/internal/ingest/reddit-ingest-manual.ts
+++ packages/functions/src/functions/internal/ingest/reddit-ingest-manual.ts
@@ -3,28 +3,34 @@ import { RedditService } from "@lib/services/reddit-service";
 export const handler = async () => {
   console.log("Hello world");
   const redditService = new RedditService();
-  const posts = await redditService.searchSubmissions("MechanicalKeyboards", {
+
+  let count = 0;
+  const postList = [];
+
+  let posts = await redditService.searchSubmissions("MechanicalKeyboards", {
     q: 'flair:"Group Buy"',
     limit: 5,
     sort: "new",
     restrict_sr: true,
   });
 
-  const post = posts.data.children[0]?.data;
+  postList.push(...posts.data.children);
 
-  if (post) {
-    console.log(post.title);
+  let after = posts.data.after;
 
-    const commentResponse = await redditService.getSubmissionComments(
-      "MechanicalKeyboards",
-      post.id,
-      { limit: 5, sort: "top" },
-    );
+  while (after && count < 5) {
+    posts = await redditService.searchSubmissions("MechanicalKeyboards", {
+      q: 'flair:"Group Buy"',
+      limit: 15,
+      sort: "new",
+      restrict_sr: true,
+      after,
+    });
 
-    console.log("Other title", commentResponse[0].data.children[0]?.data.title);
-
-    for (const comment of commentResponse[1].data.children) {
-      console.log(comment.data.body);
-    }
+    postList.push(...posts.data.children);
+    after = posts.data.after;
+    count++;
   }
+
+  console.log(postList.map((post) => post.data.title));
 };
